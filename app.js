55d4bc9251341d13bb4f69288d9ccf719e0fa502
Barri <info@bdesignstudio.co.uk>
2020-06-10 12:46:30 +0100

    Delete function

--- app.js
+++ app.js
@@ -3,11 +3,55 @@
 const http = new easyHTTP;
 
 // Get Posts-- function below is what we declared in the easyHttp as callback function
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-  if(err){
-    console.log(err)
-  } else{
-    console.log(posts);
-  }
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if(err){
+//     console.log(err)
+//   } else{
+//     console.log(posts);
+//   }
  
-});
+// });
+// Get single post --add /1 in the end of url
+
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, posts) {
+//   if(err){
+//     console.log(err);
+//   } else{
+//     console.log(post);
+//   }
+//  });
+
+// POST Request
+// Create Data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+//Create Post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+//Update Post
+
+// http.put('https://jsonplaceholder.typicode.com/posts/1',data, function(err, post){
+//   if(err){
+//         console.log(err);
+//       } else {
+//         console.log(post);
+//       }
+// });
+
+//Delete post
+  http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
+  if(err){
+    console.log(err);
+  } else {
+    console.log(response);
+  }
+ });
--- easyHttp.js
+++ easyHttp.js
@@ -1,31 +1,71 @@
 // ES5 prototype and ajax
 // -1 make constructor
 function easyHTTP() {
-this.http = new XMLHttpRequest();
+  this.http = new XMLHttpRequest();
 
 };
 
 //-2 Make an HTTP Get Request
-easyHTTP.prototype.get = function(url, callback){
-this.http.open('GET', url , true);
+easyHTTP.prototype.get = function (url, callback) {
+  this.http.open('GET', url, true);
 
-let self = this; 
-// this.http.onload = function(){
-//   //this.http.status is not in scope with the function above so we need to add let and use self 
-//   if(this.http.status === 200){
-//     console.log(this.http.responseText)
-//   }
-// };
+  let self = this;
+  // this.http.onload = function(){
+  //   //this.http.status is not in scope with the function above so we need to add let and use self 
+  //   if(this.http.status === 200){
+  //     console.log(this.http.responseText)
+  //   }
+  // };
 
-this.http.onload = function(){
-  if(self.http.status === 200){
-    callback( null, self.http.responseText); // we put null for the error
-  } else {
-    callback('Error: ' + self.http.status);
-  }
-};
-this.http.send();
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+      callback(null, self.http.responseText); // we put null for the error
+    } else {
+      callback('Error: ' + self.http.status);
+    }
+  };
+  this.http.send();
 }
-//-3 Make an HTTP POST Request
+//-3 Make an HTTP POST Request-- we have data here for posting
+//1-3Because data here is a javascript object we need to convert it to jason file so in send we stringfying it
+//2-3 we need to set the content type before onload
+easyHTTP.prototype.post = function (url, data, callback) {
+  this.http.open('POST', url, true);
+  //2-3
+  this.http.setRequestHeader('Content-type', 'application/json');
+  //3-3 onloading
+  let self = this;
+  this.http.onload = function() {
+    //response text should be the new post with the new id 
+    callback(null, self.http.responseText);
+  }
+  //1-3
+  this.http.send(JSON.stringify(data));
+}
 //-4 Make an HTTP PUT Request
+easyHTTP.prototype.put = function (url, data, callback) {
+  this.http.open('PUT', url, true);
+  //2-3
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.http.onload = function() {
+    //response text should be the new post with the new id 
+    callback(null, self.http.responseText);
+  }
+  //1-3
+  this.http.send(JSON.stringify(data));
+}
 //-5 Make an HTTP DELETE Request
+easyHTTP.prototype.delete = function (url, callback) {
+  this.http.open('DELETE', url, true);
+  let self = this;
+  this.http.onload = function() {
+    if (self.http.status === 200) {
+      callback(null,'Post Deleted'); // we put null 
+    } else {
+      callback('Error: ' + self.http.status);
+    }
+  };
+  this.http.send();
+}
